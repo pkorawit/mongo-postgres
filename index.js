b14946eadf5299d317805193f8cad17e20e5835e
Korawit Prutsachainimmit <korawit.p@gmail.com>
2019-08-02 22:24:32 +0700

    Save to DB  but have escape char problem

--- index.js
+++ index.js
@@ -16,12 +16,27 @@ MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
     var query = { "media.taken_at_timestamp": { "$gte": 1543597201, $lt: 1553965201 } };
     var i = 0;
     dbo.collection("instagrams").find(query).limit(1000).forEach(function (doc) {
+
+        const shortcode = doc.shortcode;
+        const full_name = (doc.media.owner.full_name) ? doc.media.owner.full_name : '';
+        const username = (doc.media.owner.username) ? doc.media.owner.username : '';
+        const location_id = (doc.media.location) ? doc.media.location.id : null;
+        const location_name = (doc.media.location) ? doc.media.location.name : '';
+        const like_count = (doc.media.edge_media_preview_like.count) ? doc.media.edge_media_preview_like.count : 0;
+        const taken_at_timestamp = doc.media.taken_at_timestamp;
+        let caption_text = '';
+        if(doc.media.edge_media_to_caption){
+            if(doc.media.edge_media_to_caption.edges){
+                if(doc.media.edge_media_to_caption.edges.length > 0){
+                    caption_text = doc.media.edge_media_to_caption.edges[0].node.text
+                };
+            };
+        }
         //insert to db
         var query = `INSERT INTO post (
             shortcode,
             full_name,
             username,
-            profile_pic_url,
             location_id,
             location_name,
             like_count,
@@ -29,28 +44,27 @@ MongoClient.connect(url, { useNewUrlParser: true }, function (err, db) {
             caption_text
             ) 
             VALUES (
-                '${doc.shortcode}',
-                '${doc.media.owner.full_name}',
-                '${doc.media.owner.username}',
-                '${doc.media.owner.profile_pic_url}',
-                ${(doc.media.location) ? doc.media.location.id : null},
-                '${(doc.media.location) ? doc.media.location.name : ''}',
-                ${doc.media.edge_media_preview_like.count},
-                ${doc.media.taken_at_timestamp},
-                '${(doc.media.edge_media_to_caption.edges.node) ? doc.media.edge_media_to_caption.edges[0].node.text : ''}'
+                '${shortcode}',
+                '${full_name}',
+                '${username}',
+                ${location_id},
+                '${location_name}',
+                ${like_count},
+                ${taken_at_timestamp},
+                '${caption_text}'
             )
             `;
-        // pool.query(query, (error, result) => {
-        //     if (error) {
-        //         console.log(++i + " " + doc.shortcode + " ERROR");
-        //     }
-        //     else{
-        //         console.log(++i + " " + doc.shortcode + " inserted");
-        //     }
-            
-        // })
-
-        console.log(++i + " " + (doc.media.edge_media_to_caption.edges) ? doc.media.edge_media_to_caption.edges[0].node.text : '');
+        pool.query(query, (error, result) => {
+            if (error) {
+                console.log(++i + " " + doc.shortcode + " ERROR " + error);                
+            }
+            else{
+                console.log(++i + " " + doc.shortcode + " inserted");
+            }
+            if(doc.shortcode == 'BsGLquRnpZR'){
+                console.log(query);
+            }
+        })
 
     }, function (err) {
         console.log(err);
